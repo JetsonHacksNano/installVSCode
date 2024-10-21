cd49eb118c2ac81668daf17cebaf0d352935da90
JetsonHacksNano <49042112+JetsonHacksNano@users.noreply.github.com>
2024-10-21 12:58:22 -0700

    Set version to 1.85.2 for Jetson Nano

--- installVSCodeWithPython.sh
+++ installVSCodeWithPython.sh
@@ -5,9 +5,9 @@
 # 
 
 # If you need a specific version of Visual Studio Code, set the version here, e.g.
-# VERSION=1.51.1
-# latest gets the latest stable version
-VERSION=latest
+# 1.85.2 is the latest version of VS Code that runs on Jetson Nano running Ubuntu 18.04
+# Newer versions of VS Code require a later version of libc, not available on the Nano
+VERSION=1.85.2
 wget -N -O vscode-linux-deb.arm64.deb https://update.code.visualstudio.com/$VERSION/linux-deb-arm64/stable
 sudo apt install ./vscode-linux-deb.arm64.deb
 
